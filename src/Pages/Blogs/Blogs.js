70471f8188e5014c421cb3cea32fc17460f5d47d
Omarfaruk4200 <emon35293@gmail.com>
2022-05-28 12:59:49 +0600

    added blogs

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Blogs = () => {
     return (
         <div>
-
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, veniam? Sequi delectus tempora quaerat sapiente? Natus est, cumque deserunt, doloremque dolorem doloribus fugiat, minus omnis quas reiciendis ratione deleniti perspiciatis.</p>
         </div>
     )
 }
